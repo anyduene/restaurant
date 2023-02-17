158c2f5ba8ae01a7af7730e91423d9b7c8044291
Illia <leosmile06@gmail.com>
2023-02-17 15:28:58 +0200

    add drinks and pancakes pages

--- src/app/services/categories.ts
+++ src/app/services/categories.ts
@@ -3,6 +3,6 @@ import { Category } from "../models/category";
 export const CATEGORIES: Category[] = [
   { id: 1, name: 'Pizza', items: 12, link: 'pizza' },
   { id: 2, name: 'Sushi', items: 10, link: 'sushi' },
-  { id: 3, name: 'Season menu', items: 5, link: 'season-menu' },
+  { id: 3, name: 'Pancakes', items: 5, link: 'pancakes' },
   { id: 4, name: 'Drinks', items: 7, link: 'drinks' }
 ];
