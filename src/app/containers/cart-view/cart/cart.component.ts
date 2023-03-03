b5faa120ea05f65b62bbad303e47d13bffbacd84
anyduene <leosmile06@gmail.com>
2023-03-03 15:52:54 +0200

    cart update

--- src/app/containers/cart-view/cart/cart.component.ts
+++ src/app/containers/cart-view/cart/cart.component.ts
@@ -83,12 +83,14 @@ export class CartComponent {
   card = new FormControl('', Validators.required);
 
 
-  getErrorMessage() {
+  getErrorMessageEmail() {
     if (this.email.hasError('required')) {
       return 'You must enter a value';
     }
-
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+  getErrorMessage() {
+    return 'You must enter a value';
   }
 
   applyPromoCode() {
@@ -101,18 +103,6 @@ export class CartComponent {
     } else {
       this.promoFound.emit();
     }
-  }
-
-  constructor(public dialog: MatDialog,
-  ) { }
-
-
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(DialogAnimationsExampleDialog, {
-      width: '250px',
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
   }
 }
 
@@ -121,105 +111,5 @@ export class CartComponent {
   templateUrl: './checkout-modal.html',
 })
 export class DialogAnimationsExampleDialog {
-  constructor(public dialogRef: MatDialogRef<DialogAnimationsExampleDialog>,
-    public dialog: MatDialog) { }
 
-
-  @Input() cartItems: CartItem[] = [];
-  @Input() totalCount!: number;
-  @Input() totalCost!: number;
-  @Input() totalDiscount!: number;
-
-  @Output() increaseCount = new EventEmitter<string>();
-  @Output() decreaseCount = new EventEmitter<string>();
-  @Output() removeDish = new EventEmitter<string>();
-  @Output() createOrder = new EventEmitter();
-  @Output() promoNotFound = new EventEmitter();
-  @Output() promoFound = new EventEmitter();
-
-  userName = '';
-
-  cookingTimeOptions = [
-    { name: 'Standard', value: 0 },
-    { name: 'Fast (+100₴)', value: 10000 },
-  ];
-  cookingTime: number = this.cookingTimeOptions[0].value;
-
-  get totalAmount() {
-    return this.totalCost + this.cookingTime;
-  }
-
-  itemsCheckout: CartItem[] = [];
-  show: boolean = false;
-
-  promo: string = '';
-  discount: number = 1;
-
-  onIncrease(link: string) {
-    this.increaseCount.emit(link);
-    console.log(this.totalDiscount)
-  }
-
-  onDecrease(link: string) {
-    this.decreaseCount.emit(link);
-    console.log(this.totalDiscount)
-  }
-
-  onRemove(link: string) {
-    this.removeDish.emit(link);
-  }
-
-  onCheckout() {
-    this.itemsCheckout = this.cartItems;
-  }
-
-  showForm() {
-    this.show = true;
-  }
-
-  closeForm() {
-    this.show = false;
-  }
-
-  order() {
-    this.createOrder.emit();
-    this.promo = '';
-  }
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-  name = new FormControl('', Validators.required);
-  surname = new FormControl('', Validators.required);
-  number = new FormControl('', Validators.required);
-  adress = new FormControl('', Validators.required);
-  card = new FormControl('', Validators.required);
-
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  applyPromoCode() {
-    const promo = PromoCodes.find(p => p.promo === this.promo);
-
-    this.discount = promo ? promo.discount : 1;
-
-    if (!promo) {
-      this.promoNotFound.emit();
-    } else {
-      this.promoFound.emit();
-    }
-  }
-
-
-  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
-    this.dialog.open(DialogAnimationsExampleDialog, {
-      width: '250px',
-      enterAnimationDuration,
-      exitAnimationDuration,
-    });
-  }
 }
